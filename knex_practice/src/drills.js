c8cfbc2d6eb0e4b4b414f51e829fe380501a6220
Hunter Kreshock <hkreshock@gmail.com>
2019-02-26 10:17:13 -0700

    drill 1 complete

---
+++ knex_practice/src/drills.js
@@ -0,0 +1,20 @@
+'use strict';
+
+require('dotenv').config();
+const knex = require('knex');
+
+const knexInstance = knex({
+  client: 'pg',
+  connection: process.env.DB_URL});
+
+function retrievingSearchTerm (searchTerm) {
+  knexInstance
+    .select('*')
+    .from('shopping_list')
+    .where('name', 'ILIKE', `${searchTerm}`)
+    .then(result => {
+      console.log(result);
+    });
+}
+
+retrievingSearchTerm('tofurkey');
